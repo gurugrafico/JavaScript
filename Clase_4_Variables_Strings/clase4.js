17d7b063eea87d2abf34933ec5f976ea636eb27b
Andrés Quiñones <xndres18@gmail.com>
2020-01-26 16:28:47 -0500

    Se crea el archivo clase4.js

---
+++ Clase_4_Variables_Strings/clase4.js
@@ -0,0 +1,9 @@
+var nombre = 'Andrés', apellido = 'Quiñones'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
