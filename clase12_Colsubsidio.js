e7b3bf83917293683cf53e8fc8b422a4c7b0cab4
Andrés Quiñones <gurugrafico@users.noreply.github.com>
2024-08-14 21:26:33 -0500

    Se agrega el archivo loops.js

--- clase12_Colsubsidio.js
+++
@@ -1,7 +0,0 @@
-let segundoNombre = "Elkin";
-
-if (segundoNombre === "Fabián" || segundoNombre === "Diego") {
-  console.log("Hola, " + segundoNombre);
-} else {
-  console.log("No sé quién eres: " + segundoNombre);
-}
---
+++ loops.js
@@ -0,0 +1,27 @@
+/* let segundoNombre = "Elkin";
+
+if (segundoNombre === "Fabián" || segundoNombre === "Diego") {
+  console.log("Hola, " + segundoNombre);
+} else {
+  console.log("No sé quién eres: " + segundoNombre);
+}
+const food = "pizza";
+
+switch (food) {
+  case "sushi":
+    console.log("Sushi is originally from Japan.");
+    break;
+  case "pizza":
+    console.log("Pizza is originally from Italy.");
+    break;
+  default:
+    console.log("I have never heard of that dish.");
+    break;
+}
+ */
+
+  let peliculas = ["Inception", "Interstellar", "The Matrix", "Joker", "Parasite", "Deadpool", "Soul"];
+
+  for (let i = 0; i < peliculas.length; i++) {
+    console.log(`Película ${i + 1}: ${peliculas[i]}`);
+  }
