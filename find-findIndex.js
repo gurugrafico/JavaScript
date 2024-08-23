f80ea2793eebd8d5cf8cbbc3c37da9392db527ff
Andrés Quiñones <xndres18@gmail.com>
2024-08-23 16:26:47 -0500

    Se modifica find.js y se agregan otros dos archivos => slice.js y spread-operator.js

--- find-findIndex.js
+++ find-findIndex.js
@@ -9,3 +9,11 @@ const firstNumberGreaterThan10 = multiplesOf5.find(number => number > 10)
 
 console.log(multiplesOf5)
 console.log(firstNumberGreaterThan10)
+
+// findIndex()
+
+const randomNumbers = [6, 14, 27, 56, 40]
+const indexNumber = randomNumbers.findIndex(number => number > 50)
+
+console.log(randomNumbers)
+console.log(indexNumber)
