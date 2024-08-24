e478a964eb36a04dc8079d9ae693ec45a1f929e1
Andrés Quiñones <xndres18@gmail.com>
2024-08-23 22:42:22 -0500

    Se agrega el archivo anatomia.js e index.js

---
+++ objetos/index.js
@@ -0,0 +1,32 @@
+/*
+
+Un objeto es una estructura de datos
+
+key / value
+
+objeto {
+    propiedad: valor,
+    propiedad: valor,
+    propiedad: valor
+}
+
+*/
+
+const persona = {
+    nombre: "Andrés",
+    edad: 31,
+    direccion: {
+        calle: "Calle 51 sur",
+        ciudad: "Madrid",
+    },
+    saludar() {
+        console.log(`Hola, mi nombre es ${persona.nombre}`);
+    },
+};
+
+console.log(persona);
+persona.saludar();
+
+persona.telefono = "3216548709";
+
+console.log(persona.telefono);
