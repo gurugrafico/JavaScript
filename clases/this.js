a0e49f626571b43095c482be57371b088fb0107f
Andrés Quiñones <xndres18@gmail.com>
2024-08-27 23:55:49 -0500

    Se modifica el archivo this.js

---
+++ clases/this.js
@@ -0,0 +1,16 @@
+/*
+
+this --- class
+
+this --- objeto -- class
+
+*/
+
+class Persona {
+    constructor (nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
+
+const persona1 = new Persona("Alice", 25)
