e2b33b4010f2bf4b588c1f979bc45d2da4785338
Andrés Quiñones <gurugrafico@users.noreply.github.com>
2024-09-06 12:49:52 -0500

    Se agregan las opciones de la izquierda en la barra de navegación

--- proyecto-react/test-react/src/Components/Navbar/index.jsx
+++ proyecto-react/test-react/src/Components/Navbar/index.jsx
@@ -1,7 +1,36 @@
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-
+    <nav>
+        <ui>
+            <li>
+                <NavLink to="/">Shop</NavLink>
+            </li>
+            <li>
+                <NavLink to="/clothes">Ropa</NavLink>
+            </li>
+            <li>
+                <NavLink to="/electronics">Electrónicos</NavLink>
+            </li>
+            <li>
+                <NavLink to="/other">Otros</NavLink>
+            </li>
+        </ui>
+        <ui>
+            <li>
+                <NavLink to="/">Shop</NavLink>
+            </li>
+            <li>
+                <NavLink to="/clothes">Ropa</NavLink>
+            </li>
+            <li>
+                <NavLink to="/electronics">Electrónicos</NavLink>
+            </li>
+            <li>
+                <NavLink to="/other">Otros</NavLink>
+            </li>
+        </ui>
+    </nav>
 }
 
 export default Navbar
