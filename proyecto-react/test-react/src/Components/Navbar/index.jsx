fe371716a438d183be2cc69efe11cedbc5bf3ce9
Andrés Quiñones <gurugrafico@users.noreply.github.com>
2024-09-08 10:58:58 -0500

    Se retorna el Navbar en la función

--- proyecto-react/test-react/src/Components/Navbar/index.jsx
+++ proyecto-react/test-react/src/Components/Navbar/index.jsx
@@ -1,33 +1,35 @@
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    <nav>
-        <ui>
-            <li>
-                <NavLink to="/">Shop</NavLink>
-            </li>
-            <li>
-                <NavLink to="/clothes">Ropa</NavLink>
-            </li>
-            <li>
-                <NavLink to="/electronics">Electrónicos</NavLink>
-            </li>
-            <li>
-                <NavLink to="/other">Otros</NavLink>
-            </li>
-        </ui>
-        <ui>
-            <li>
-                <NavLink to="/my-account">Mi cuenta</NavLink>
-            </li>
-            <li>
-                <NavLink to="/my-orders">Mis órdenes</NavLink>
-            </li>
-            <li>
-                <NavLink to="/sign-in">Registrarse</NavLink>
-            </li>
-        </ui>
-    </nav>
+    return (
+        <nav>
+            <ui>
+                <li>
+                    <NavLink to="/">Shop</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/clothes">Ropa</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/electronics">Electrónicos</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/other">Otros</NavLink>
+                </li>
+            </ui>
+            <ui>
+                <li>
+                    <NavLink to="/my-account">Mi cuenta</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/my-orders">Mis órdenes</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/sign-in">Registrarse</NavLink>
+                </li>
+            </ui>
+        </nav>
+    )    
 }
 
 export default Navbar
