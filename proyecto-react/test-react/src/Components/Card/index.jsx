260b122aed54d17dd0813ad8470a18d322d08b11
Andrés Quiñones <gurugrafico@users.noreply.github.com>
2024-09-16 17:14:25 -0500

    Se hace el llamado del API para mostrar los productos

--- proyecto-react/test-react/src/Components/Card/index.jsx
+++ proyecto-react/test-react/src/Components/Card/index.jsx
@@ -5,11 +5,11 @@ const Card = () => {
                 <span className="absolute bottom-0 left-0 bg-white/60 text-black text-xs m-2 px-3 py-0.5">Categoría</span>
                 <img className="w-full h-full object-cover rounded-lg"
                 src="https://images.pexels.com/photos/245035/pexels-photo-245035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="imagen"/>
-                <div className="absolute top-0 right-0 flex justify-center bg-white w-6 h-6 rounded-full m-2 p-1">+</div>
+                <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">+</div>
             </figure>
-            <p>
-                <span>Nombre del artículo</span>
-                <span>Precio</span>
+            <p className="flex justify-between">
+                <span className="text-sm font-light">Nombre del artículo</span>
+                <span className="text-lg font-medium">Precio</span>
             </p>
         </div>
     )
--- proyecto-react/test-react/src/Pages/Home/index.jsx
+++ proyecto-react/test-react/src/Pages/Home/index.jsx
@@ -1,12 +1,23 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 
 function Home() {
 
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+    .then((response) => response.json())
+    .then((data) => setItems(data))
+  }, [])
+
   return (
     <Layout className='bg-orange-600'>
       Inicio 🏠
-      <Card></Card>
+      {items?.map(() => (
+        <Card/>
+      ))}
     </Layout>
   )
 }
