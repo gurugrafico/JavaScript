898c8dcc342b24b7877d578dbeebf9f8de4c7f3c
Andrés Quiñones <xndres18@gmail.com>
2024-09-18 14:15:01 -0500

    Commit

--- proyecto-react/test-react/src/Pages/App/index.jsx
+++ proyecto-react/test-react/src/Pages/App/index.jsx
@@ -1,5 +1,6 @@
 import { useRoutes, BrowserRouter } from 'react-router-dom';
 import { ShoppingCartProvider } from '../../Context';
+import { ShoppingCartContext } from '../../Context';
 import Home from '../Home';
 import MyAccount from '../MyAccount';
 import MyOrder from '../MyOrder';
